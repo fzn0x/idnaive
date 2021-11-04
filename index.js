7e871022c37e342845835197dd177307e188670c
fncolon <fncolon@pm.me>
2021-11-04 23:04:06 +0700

    feat: add ESM support #1

--- index.js
+++ index.js
@@ -1,16 +1,129 @@
-const NaiveBayes = require("./src/naive-bayes.js");
+const { enStem, idStem } = require("./lib/stemmer");
 
-const naiveBayes = new NaiveBayes("en");
+class NaiveBayes {
+  constructor(languageCode) {
+    this.languageCode = languageCode;
+    this.wordsData = {};
+    this.labels = {};
+    this.frequencyCount = {};
+  }
 
-const datasets = [
-  ["You're doing good and expressive", "positive"],
-  ["You're doing wow and nice", "positive"],
-  ["What is this? very waste of time, and bad", "negative"],
-];
+  _init(data, label) {
+    if (!this.wordsData[data]) {
+      this.wordsData[data] = {};
+    }
+    if (!this.wordsData[data][label]) {
+      this.wordsData[data][label] = 0;
 
-naiveBayes.learn(...datasets);
+      if (!this.labels[label]) {
+        this.labels[label] = 0;
+      }
 
-let result = naiveBayes.classify(
-  "This is bad, and you should try better because it waste my time"
-);
-console.log(result);
+      if (!this.frequencyCount[label]) {
+        this.frequencyCount[label] = {};
+      }
+    }
+  }
+
+  learn(...args) {
+    args.forEach((value) => {
+      let data = this._stem(value[0]);
+      let label = value.pop();
+      this._init(data, label);
+
+      this.wordsData[data][label]++;
+      this.labels[label]++;
+
+      let tokenizer = this._filter(data);
+
+      tokenizer.forEach((word) => {
+        let frequencyTable = this._frequencyTable(tokenizer);
+
+        if (!this.frequencyCount[label][word]) {
+          this.frequencyCount[label][word] = frequencyTable[word];
+        } else {
+          this.frequencyCount[label][word] += frequencyTable[word];
+        }
+      });
+    });
+  }
+
+  classify(data) {
+    let filteredData = this._filter(data);
+    let result = this._findProbability(filteredData);
+    return result;
+  }
+
+  _stem(str) {
+    switch (this.languageCode) {
+      case "en":
+        return enStem(str);
+        break;
+      case "id":
+        return idStem(str);
+        break;
+      default:
+        return enStem(str);
+        break;
+    }
+  }
+
+  _filter(str) {
+    let stemmed = this._stem(str);
+    let outString = stemmed.replace(
+      /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,
+      ""
+    );
+    let punctuation = /[^(a-zA-ZA-Яa-я0-9_)+\s]/g;
+    let sanitizedWord = outString.replace(punctuation, " ");
+    return sanitizedWord.split(/\s+/);
+  }
+
+  _findProbability(filteredData) {
+    let self = this;
+    let maxProbability = -Infinity;
+    let chosenLabel = null;
+
+    let frequencyTable = self._frequencyTable(filteredData);
+
+    Object.keys(self.labels).forEach(function (label) {
+      let labelProbability =
+        self.labels[label] / Object.keys(self.wordsData).length;
+
+      let lastProbability = Math.log(labelProbability);
+
+      Object.keys(frequencyTable).forEach(function (word) {
+        let frequencyInText = frequencyTable[word];
+        let wordProbability = self._wordProbability(word, label);
+
+        lastProbability += frequencyInText * Math.log(wordProbability);
+      });
+
+      if (lastProbability > maxProbability) {
+        maxProbability = lastProbability;
+        chosenLabel = label;
+      }
+    });
+
+    return chosenLabel;
+  }
+
+  _wordProbability(word, label) {
+    let frequencyCount = this.frequencyCount[label][word] || 0;
+    let wordCount = this.labels[label];
+    return (
+      (frequencyCount + 1) / (wordCount + Object.keys(this.wordsData).length)
+    );
+  }
+
+  _frequencyTable(words) {
+    let frequencyTable = {};
+    words.forEach(function (word) {
+      if (!frequencyTable[word]) frequencyTable[word] = 1;
+      else frequencyTable[word]++;
+    });
+    return frequencyTable;
+  }
+}
+
+exports = module.exports = NaiveBayes;
