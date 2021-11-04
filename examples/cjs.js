64c221d70cb67dbe503e94ec54635767ce1649ca
fncolon <fncolon@pm.me>
2021-11-04 23:01:20 +0700

    chore(idnaive): restructure project

---
+++ examples/cjs.js
@@ -0,0 +1,16 @@
+const NaiveBayes = require("../index.js");
+
+const naiveBayes = new NaiveBayes("en");
+
+const datasets = [
+  ["You're doing good and expressive", "positive"],
+  ["You're doing wow and nice", "positive"],
+  ["What is this? very waste of time, and bad", "negative"],
+];
+
+naiveBayes.learn(...datasets);
+
+let result = naiveBayes.classify(
+  "This is bad, and you should try better because it waste my time"
+);
+console.log(result);
